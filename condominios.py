0d062bb57977143cf34cada776cf29c7ecf0ee69
Bleine <bleinee@gmail.com>
2026-03-18 19:02:44 -0300

    Tratamento de erros - try e except

--- condominios.py
+++ condominios.py
@@ -3,17 +3,19 @@ def condominios():
         print("Você selecionou a opção CONDOMÍNIOS")
         print("Opção 1 - Criar \n",  "Opção 2 - Ler \n",
               "Opção 3 - Atualizar \n", "Opção 4 - Deletar \n", "Opção 0 - Voltar \n")
-
-        escolha = int(input("Escolha uma opção: "))
-
-        match escolha:
-            case 1:
-                print("CREATE")
-            case 2:
-                print("READ")
-            case 3:
-                print("UPDATE")
-            case 4:
-                print("DELETE")
-            case 0:
-                break
+        try:
+            escolha = int(input("Escolha uma opção: "))
+        except ValueError:
+            print("Escolha não é um inteiro")
+        else:
+            match escolha:
+                case 1:
+                    print("CREATE")
+                case 2:
+                    print("READ")
+                case 3:
+                    print("UPDATE")
+                case 4:
+                    print("DELETE")
+                case 0:
+                    break
